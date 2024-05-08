3840f4953c111a2c068b25e77c5651ce2532f4b0
Alexsander <sparco@nxt.ru>
2024-05-08 13:27:05 +0500

    correct dz

--- src/js/character.js
+++ src/js/character.js
@@ -9,20 +9,25 @@
     ];
 
     constructor(name, type, attack, defence) {
-       this.name = name;
-       if((this.name).length < 2 || (this.name).length > 10) {
+       
+       if((name).length < 2 || (name).length > 10) {
         throw new Error ('Invalid length name');
-       }else if(typeof this.name !== 'string') {
+       }
+       
+       if(typeof name !== 'string') {
         throw new Error ('Name is not string');
        }
 
-       this.type = type;
        if(!Character.typeContainer.includes(type)) {
         throw new Error ('Invalid type of character');
        }
-        
+
+        this.name = name;
+        this.type = type;
+       
         this.health = 100;
         this.level = 1;
+        
         this.attack = attack;
         this.defence = defence;
     }
